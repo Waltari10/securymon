dd3308a548fceb1d8a578602f715307ca9cab9cb
Valtteri Laine <valtteri.e.laine@gmail.com>
2019-11-16 18:32:26 +0200

    adds build.

---
+++ docs/precache-manifest.21fafd0661528e9457c3d8a81e9998a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66b6eec9263cbbee75a74738adb61cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c4b02e6a576a63e57e",
+    "url": "/static/css/main.4a95bed0.chunk.css"
+  },
+  {
+    "revision": "48e2a20454080f9abf3c",
+    "url": "/static/js/2.20cd7c00.chunk.js"
+  },
+  {
+    "revision": "27c4b02e6a576a63e57e",
+    "url": "/static/js/main.39f49608.chunk.js"
+  },
+  {
+    "revision": "3fe6e2482fd921eb7c93",
+    "url": "/static/js/runtime-main.a16e6991.js"
+  },
+  {
+    "revision": "2109129a5c2a0119fdd28edcfd148276",
+    "url": "/static/media/Answer1.2109129a.png"
+  },
+  {
+    "revision": "7198140604ac9cbf348603abf8e2f7c7",
+    "url": "/static/media/Card1.71981406.png"
+  },
+  {
+    "revision": "b49a3a5998fcedfb50dc1fe6a67c8234",
+    "url": "/static/media/Morl-Bold.b49a3a59.otf"
+  },
+  {
+    "revision": "73ca1d63c96bcfa7cd5c131e20c596f8",
+    "url": "/static/media/Morl-Regular.73ca1d63.otf"
+  }
+]);
