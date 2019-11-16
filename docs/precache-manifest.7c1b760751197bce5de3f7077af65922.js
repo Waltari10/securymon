2ee98b098e7113eb81fbaeb0dacce4d081745415
Valtteri Laine <valtteri.e.laine@gmail.com>
2019-11-16 23:00:52 +0200

    build

---
+++ docs/precache-manifest.7c1b760751197bce5de3f7077af65922.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74220fce6771f133e93f5b86d671ef84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6e7eb464cb97cb5b22e",
+    "url": "./static/css/main.1dbbf29e.chunk.css"
+  },
+  {
+    "revision": "d8e99363e5418fdb4db3",
+    "url": "./static/js/2.4339f3da.chunk.js"
+  },
+  {
+    "revision": "f6e7eb464cb97cb5b22e",
+    "url": "./static/js/main.a8af681a.chunk.js"
+  },
+  {
+    "revision": "357ce23fca03be8cd818",
+    "url": "./static/js/runtime-main.07fe134a.js"
+  },
+  {
+    "revision": "0040a943a9ddfa13f5e5db719e32b4a9",
+    "url": "./static/media/Answer1.0040a943.png"
+  },
+  {
+    "revision": "6c4882c53aaa3a5cb872d1362c649d77",
+    "url": "./static/media/Card1.6c4882c5.png"
+  },
+  {
+    "revision": "b49a3a5998fcedfb50dc1fe6a67c8234",
+    "url": "./static/media/Morl-Bold.b49a3a59.otf"
+  },
+  {
+    "revision": "73ca1d63c96bcfa7cd5c131e20c596f8",
+    "url": "./static/media/Morl-Regular.73ca1d63.otf"
+  },
+  {
+    "revision": "382adb847acc90a48c3c21273a1d9a35",
+    "url": "./static/media/board.382adb84.png"
+  },
+  {
+    "revision": "cec1fe305557e25545f068847943ba8a",
+    "url": "./static/media/logo_long.cec1fe30.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a1368761a8574497db0176baef9a3152.js"
+  "./precache-manifest.7c1b760751197bce5de3f7077af65922.js"
 );
 
 self.addEventListener('message', (event) => {
